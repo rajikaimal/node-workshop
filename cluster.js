291f149835b576a03d02296aeadc8aa64f84a5a0
rajikaimal <inbox.rajika@gmail.com>
2019-04-12 23:59:58 +0530

    Space

--- cluster.js
+++ cluster.js
@@ -14,7 +14,7 @@ if (cluster.isMaster) {
   });
 } else {
   // Workers can share any TCP connection
-  // In this case it is an expressserver
+  // In this case it is an express express server
   require("./index");
 
   console.log(`Worker ${process.pid} started`);
